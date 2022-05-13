db118c0a62b322552d3abbca3a08a9cf7731c5d8
dhfherna <dhfherna@bancolombia.com.co>
2022-05-13 11:22:51 -0500

    Create a project test

---
+++ src/test/java/co/com/nerfo/testing/RepositoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nerfo.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
