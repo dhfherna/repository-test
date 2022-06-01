4b60d1b6d81bf5b4f5c9717521e00a3c85541089
dhfherna <104927849+dhfherna@users.noreply.github.com>
2022-06-01 11:21:14 -0500

    Dhfherna patch 1 (#3)
    * Create secret-scanning.yml
    
    * Test secret scanning tool
    
    * Update secret-scanning.yml
    
    * Delete secret test

--- src/main/java/co/com/nerfo/testing/RepositoryTestApplication.java
+++ src/main/java/co/com/nerfo/testing/RepositoryTestApplication.java
@@ -1,13 +1,57 @@
 package co.com.nerfo.testing;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.opensearch.action.get.GetRequest;
+import org.opensearch.action.get.GetResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestClientBuilder;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class RepositoryTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepositoryTestApplication.class, args);
+		try {
+			//SpringApplication.run(RepositoryTestApplication.class, args);
+			//Point to keystore with appropriate certificates for security.
+			System.setProperty("javax.net.ssl.trustStore", "C:/Users/Michael Page/Documents/MisNotas/CertificadosWeb/Bancolombia_TrustStore.jks");
+			System.setProperty("javax.net.ssl.trustStorePassword", "Clave123*");
+
+			//Establish credentials to use basic authentication.
+			//Only for demo purposes. Don't specify your credentials in code.
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+			credentialsProvider.setCredentials(AuthScope.ANY,
+					new UsernamePasswordCredentials("opensource", "User_elastic2021"));
+
+			//Create a client.
+			RestClientBuilder builder = RestClient.builder(new HttpHost("emma-vault.apps.bancolombia.com", 443, "https"))
+					.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+						@Override
+						public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+							return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+						}
+					});
+			RestHighLevelClient client = new RestHighLevelClient(builder);
+
+			//Getting back the document
+			GetRequest getRequest = new GetRequest("open_metrics", "1");
+			GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
+
+			System.out.println(response.getSourceAsString());
+			client.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
